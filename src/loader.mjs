6a0b1c414edffdd6a252e8cef926d9277d5ce566
romap0 <pavlov@pqlab.ru>
2018-09-21 14:26:40 +0300

    Fix eslint errors

--- src/loader.mjs
+++ src/loader.mjs
@@ -1,7 +1,8 @@
+// eslint-disable-next-line import/prefer-default-export
 export function resolve(specifier, parentModuleURL, defaultResolve) {
   let base = parentModuleURL;
-  if (base === 'file://' + process.env.MOCHA_COPY_FILE) {
-    base = 'file://' + process.env.MOCHA_BASE_FILE;
+  if (base === `file://${process.env.MOCHA_COPY_FILE}`) {
+    base = `file://${process.env.MOCHA_BASE_FILE}`;
   }
 
   return defaultResolve(specifier, base);
